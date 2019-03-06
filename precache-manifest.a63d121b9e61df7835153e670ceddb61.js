b627dfa1aa4b1c19c809286db1d76aeea6a2660d
Juwon Hong <juwonhp373@gmail.com>
2019-03-06 21:55:57 +0900

    add dist

---
+++ precache-manifest.a63d121b9e61df7835153e670ceddb61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "db6fc2aeb9e753d4806d",
+    "url": "/js/about.9d62e569.js"
+  },
+  {
+    "revision": "0dfca5cd5ec6ee0c5103",
+    "url": "/css/app.d3753a49.css"
+  },
+  {
+    "revision": "0dfca5cd5ec6ee0c5103",
+    "url": "/js/app.13afcf18.js"
+  },
+  {
+    "revision": "f806551c1f5908d6c33e",
+    "url": "/css/chunk-vendors.e274979e.css"
+  },
+  {
+    "revision": "f806551c1f5908d6c33e",
+    "url": "/js/chunk-vendors.82a02982.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b2ae091477044c9db120f206c800dad7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
